87977af0afbcad8020d51abe92a8f74a86acc95f
aofleejay <aofleejay@hotmail.com>
2020-10-09 02:45:08 +0700

    :sparkles: Cors.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const Koa = require("koa");
 const Router = require("koa-router");
+const cors = require("@koa/cors");
 const rateContoller = require("./controllers/rate");
 const symbolController = require("./controllers/symbol");
 
@@ -10,6 +11,7 @@ router.get("/ping", (ctx) => (ctx.body = "pong"));
 router.get("/symbols", symbolController.get);
 router.get("/rates", rateContoller.get);
 
+app.use(cors());
 app.use(router.routes()).use(router.allowedMethods());
 
 module.exports = app;
